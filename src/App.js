5ea5b795f6f84081e43326a5520228996106096d
Gaston Ozuna <gastonozuna11@gmail.com>
2022-01-31 20:54:15 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tienda Online Ozuna
+          Proximamente Tienda Online Ozuna
         </p>
         <a
           className="App-link"
